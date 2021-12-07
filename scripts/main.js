7bde8a107814446b5a6dda2e2926740585e15a90
CyanForWeb <95006417+CyanForWeb@users.noreply.github.com>
2021-12-07 19:16:02 +0900

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -7,8 +7,8 @@ let myImage = document.querySelector('img');
 
 myImage.onclick = function () {
   let mySrc = myImage.getAttribute('src');
-  if(mySrc === 'MyDog.png') {
-    myImage.setAttribute ('src','images/firefox2.png');
+  if(mySrc === 'images/firefox-icon.png') {
+    myImage.setAttribute ('src','MyDog.png');
   } else {
     myImage.setAttribute ('src','images/firefox-icon.png');
   }
