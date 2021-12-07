557a1d8e1595025336827883acc14a778f51c001
CyanForWeb <95006417+CyanForWeb@users.noreply.github.com>
2021-12-07 19:06:40 +0900

    サンプルページの動作変更

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,11 @@
 // Image switcher code
 
 let myImage = document.querySelector('img');
+//documentはオブジェクト。
+//ブラウザが表示しているページ。DDMとも呼ぶ。
+//querySelectorはページの一部（HTML要素）を呼ぶ関数。
 
-myImage.onclick = function() {
+myImage.onclick = function () {
   let mySrc = myImage.getAttribute('src');
   if(mySrc === 'images/firefox-icon.png') {
     myImage.setAttribute ('src','images/firefox2.png');
@@ -15,22 +18,26 @@ myImage.onclick = function() {
 
 let myButton = document.querySelector('button');
 let myHeading = document.querySelector('h1');
+console.log(myHeading);
 
 function setUserName() {
   let myName = prompt('Please enter your name.');
+  //promptは、組み込みの関数。prompt Windowを表示して、ユーザ入力をさせる。
   if(!myName) {
     setUserName();
   } else {
     localStorage.setItem('name', myName);
-    myHeading.innerHTML = 'Mozilla is cool, ' + myName;
+    //入力データを、特別な場所（変数でない、ファイルなどに相当）に保存する。
+    myHeading.innerHTML = 'こんにちは, ' + myName;
   }
 }
 
 if(!localStorage.getItem('name')) {
+  //以前に特別な場所（ファイルなど）保存していた名前があれば、それを使う。
   setUserName();
 } else {
   let storedName = localStorage.getItem('name');
-  myHeading.innerHTML = 'Mozilla is cool, ' + storedName;
+  myHeading.innerHTML = 'さようなら, ' + storedName;
 }
 
 myButton.onclick = function() {
